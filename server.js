29d84b8fafddaa9ad9a140b4e4ade0fee8f74947
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2022-07-13 15:50:31 +0530

    update Subject Group Chat socket id

--- server.js
+++ server.js
@@ -12746,7 +12746,7 @@ const server = app.listen(port, function () {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     // credentials: true,
   },
 });

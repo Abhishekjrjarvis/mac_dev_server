2c90ab2c3b89db15952d80fa3233389c19e298e8
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2022-07-22 14:46:56 +0530

    update server production servers and complaint and Transfer and Chat servers

--- server.js
+++ server.js
@@ -192,9 +192,9 @@ app.use(express.json());
 app.use(
   cors({
     // origin: "http://18.205.27.165",
-    // origin: ["http://18.205.27.165", "http://localhost:3000"],
+    origin: ["http://18.205.27.165", "http://localhost:3000", "https://qviple.com"],
     // origin: "http://localhost:3000",
-    origin: "https://qviple.com",
+    // origin: "https://qviple.com",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })

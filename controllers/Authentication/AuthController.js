635aff2ffa5228a9005ee3bfe312e92794557a72
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2022-08-22 14:26:00 +0530

    update google signin and location permission 1.4

--- controllers/Authentication/AuthController.js
+++ controllers/Authentication/AuthController.js
@@ -548,7 +548,8 @@ module.exports.authenticationGoogle = async (req, res) =>{
     const user = await User.findOne({ userEmail: email})
     .select('userLegalName username userEmail deviceToken profilePhoto photoId')
     if(user){
-      res.status(200).send({ message: 'successfully signed In', sign_in: true, user: user})
+      const token = generateAccessToken(user?.username, user?._id);
+      res.status(200).send({ message: 'successfully signed In', sign_in: true, user: user, token: token})
     }
     else{
       res.status(200).send({ message: 'Failed to signed In', sign_in: false})

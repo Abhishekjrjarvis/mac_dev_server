67ea2e36840d81afecc935d9f287d9058dde9301
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2022-09-15 13:00:44 +0530

    update development server 18 13:00

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ const filterNew = require("./routes/Filterization/filterRoute");
 const dburl = `${process.env.DB_URL2}`; // Development
 // const dburl = `${process.env.DB_URL}`; // Production
 
-// 6316f3cf8ae18d300d11641e - Development
+// 6322d41f510a26993fe95bc5 - Development
 // 630f3b68a5eb4786489045a1 - Production
 
 mongoose

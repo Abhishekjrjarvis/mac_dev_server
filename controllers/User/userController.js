2b74b744700965adab56052438eb52e96774399c
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2022-11-15 17:20:57 +0530

    fix razorpay module issue again admission start, end seats ins block

--- controllers/User/userController.js
+++ controllers/User/userController.js
@@ -32,7 +32,7 @@ exports.retrieveProfileData = async (req, res) => {
     const { id } = req.params;
     var totalUpVote = 0;
     const user = await User.findById({ _id: id }).select(
-      "userLegalName photoId show_suggestion is_mentor questionCount blockedBy blockCount blockStatus user one_line_about recoveryMail answerQuestionCount recentChat profilePhoto user_birth_privacy user_address_privacy user_circle_privacy tag_privacy user_follower_notify user_comment_notify user_answer_notify user_institute_notify userBio userAddress userEducation userHobbies userGender coverId profileCoverPhoto username followerCount followingUICount circleCount postCount userAbout userEmail userAddress userDateOfBirth userPhoneNumber userHobbies userEducation "
+      "userLegalName photoId show_suggestion is_mentor user_block_institute questionCount blockedBy blockCount blockStatus user one_line_about recoveryMail answerQuestionCount recentChat profilePhoto user_birth_privacy user_address_privacy user_circle_privacy tag_privacy user_follower_notify user_comment_notify user_answer_notify user_institute_notify userBio userAddress userEducation userHobbies userGender coverId profileCoverPhoto username followerCount followingUICount circleCount postCount userAbout userEmail userAddress userDateOfBirth userPhoneNumber userHobbies userEducation "
     );
     const answers = await Answer.find({ author: id });
     for (let up of answers) {
@@ -1081,7 +1081,7 @@ exports.getDashDataQuery = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById({ _id: id }).select(
-      "userLegalName username userBlock ageRestrict blockedBy is_mentor show_suggestion photoId blockStatus one_line_about profilePhoto user_birth_privacy user_address_privacy user_circle_privacy tag_privacy user_follower_notify user_comment_notify user_answer_notify user_institute_notify"
+      "userLegalName username userBlock user_block_institute ageRestrict blockedBy is_mentor show_suggestion photoId blockStatus one_line_about profilePhoto user_birth_privacy user_address_privacy user_circle_privacy tag_privacy user_follower_notify user_comment_notify user_answer_notify user_institute_notify"
     );
     if (user.userPosts && user.userPosts.length < 1) {
       var post = [];

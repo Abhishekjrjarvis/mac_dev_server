ae2160a36a22b9f189601332f3efd135e4bb8796
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2022-12-01 13:21:53 +0530

    update server

--- controllers/User/userController.js
+++ controllers/User/userController.js
@@ -47,14 +47,14 @@ exports.retrieveProfileData = async (req, res) => {
       upVote: totalUpVote,
       post,
     });
-    if(`${req.tokenData?.userId}` === `${id}`){
-
-    }
-    else{
-      const see_user = await User.findById({_id: `${req.tokenData?.userId}`}).select('userLegalName deviceToken')
+    if (`${req.tokenData?.userId}` === `${id}`) {
+    } else {
+      const see_user = await User.findById({
+        _id: `${req.tokenData?.userId}`,
+      }).select("userLegalName deviceToken");
       invokeSpecificRegister(
         "Specific Notification",
-        `${see_user?.username} viewed your profile`,
+        `${see_user?.userLegalName} viewed your profile`,
         "View Profile",
         see_user._id,
         see_user.deviceToken

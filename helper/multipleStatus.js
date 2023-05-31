91c3ae675f7664236180d450f71f59901b97565a
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2023-05-31 08:17:19 +0530

    update dev backend code base with slight changes in Direct Excel joining student version -87

--- helper/multipleStatus.js
+++ helper/multipleStatus.js
@@ -569,7 +569,7 @@ exports.fee_reordering_direct_student_payload = async (
             appId: apply._id,
             applicable_fee: fee_structure?.total_admission_fees,
           });
-          // console.log("card created");
+          console.log("card created");
           for (var nest of ref?.remain_array) {
             const s_admin = await Admin.findById({
               _id: `${process.env.S_ADMIN_ID}`,
@@ -719,7 +719,7 @@ exports.fee_reordering_direct_student_payload = async (
             appId: apply._id,
             applicable_fee: total_amount,
           });
-          // console.log("card created");
+          console.log("card created");
           for (var nest of ref?.remain_array) {
             const s_admin = await Admin.findById({
               _id: `${process.env.S_ADMIN_ID}`,

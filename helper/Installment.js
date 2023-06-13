8cb260bac01d7484293bc35a6450e3b3d6d11aaa
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2023-06-13 18:43:29 +0530

    update data

--- helper/Installment.js
+++ helper/Installment.js
@@ -1753,6 +1753,7 @@ exports.retro_student_heads_sequencing_query = async (
   direct_args
 ) => {
   try {
+    console.log("Remain", r_args);
     var price_query = r_args?.paid_fee;
     console.log(price_query);
     if (direct_args) {

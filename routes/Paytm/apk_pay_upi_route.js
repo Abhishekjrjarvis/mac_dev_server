753f377474bcb16b71f7cd1a53638504c2908f14
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2023-07-09 10:57:38 +0530

    update paytm

--- routes/Paytm/apk_pay_upi_route.js
+++ routes/Paytm/apk_pay_upi_route.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const {
   generatePaytmTxnToken,
   paytmVerifyResponseStatus,
-  paytmVerifyAdmissionResponseStatus,
-  paytmVerifyHostelResponseStatus,
-  paytmVerifyBacklogResponseStatus,
-  paytmVerifyTransportResponseStatus,
-  paytmVerifyParticipateResponseStatus,
-  paytmVerifyDirectAdmissionResponseStatus,
+  // paytmVerifyAdmissionResponseStatus,
+  // paytmVerifyHostelResponseStatus,
+  // paytmVerifyBacklogResponseStatus,
+  // paytmVerifyTransportResponseStatus,
+  // paytmVerifyParticipateResponseStatus,
+  // paytmVerifyDirectAdmissionResponseStatus,
 } = require("../../controllers/Paytm/pay_upi");
 const {
   generateApkPaytmTxnToken,
@@ -17,46 +17,46 @@ const {
 
 // Internal Fees Payment
 router.route("/apk/generateTxnToken").post(generateApkPaytmTxnToken);
-router
-  .route(
-    "/verify/internal/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/query/:name"
-  )
-  .post(paytmVerifyResponseStatus);
+// router
+//   .route(
+//     "/verify/internal/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/query/:name"
+//   )
+//   .post(paytmVerifyResponseStatus);
 
-// Admission Fees Payment
-router
-  .route(
-    "/verify/admission/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/install/:payment_installment/remain/:payment_remain_1/query/:name/card/:payment_card_type/status/:ad_status_id"
-  )
-  .post(paytmVerifyAdmissionResponseStatus);
+// // Admission Fees Payment
+// router
+//   .route(
+//     "/verify/admission/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/install/:payment_installment/remain/:payment_remain_1/query/:name/card/:payment_card_type/status/:ad_status_id"
+//   )
+//   .post(paytmVerifyAdmissionResponseStatus);
 
-// Hostel Fees Payment
-router
-  .route(
-    "/verify/hostel/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/install/:payment_installment/status/:ad_status_id/query/:name"
-  )
-  .post(paytmVerifyHostelResponseStatus);
+// // Hostel Fees Payment
+// router
+//   .route(
+//     "/verify/hostel/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/install/:payment_installment/status/:ad_status_id/query/:name"
+//   )
+//   .post(paytmVerifyHostelResponseStatus);
 
-// Backlog Fees Payment
-router
-  .route(
-    "/verify/backlog/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/query/:name"
-  )
-  .post(paytmVerifyBacklogResponseStatus);
+// // Backlog Fees Payment
+// router
+//   .route(
+//     "/verify/backlog/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/query/:name"
+//   )
+//   .post(paytmVerifyBacklogResponseStatus);
 
-// Transport Fees Payment
-router
-  .route(
-    "/verify/transport/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/query/:name"
-  )
-  .post(paytmVerifyTransportResponseStatus);
+// // Transport Fees Payment
+// router
+//   .route(
+//     "/verify/transport/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/query/:name"
+//   )
+//   .post(paytmVerifyTransportResponseStatus);
 
-// Participate Fees Payment
-router
-  .route(
-    "/verify/participate/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/status/:ad_status_id/query/:name"
-  )
-  .post(paytmVerifyParticipateResponseStatus);
+// // Participate Fees Payment
+// router
+//   .route(
+//     "/verify/participate/fee/:moduleId/paid/:paidBy/to/:paidTo/price/:amount_nocharges/device/:isApk/status/:ad_status_id/query/:name"
+//   )
+//   .post(paytmVerifyParticipateResponseStatus);
 
 // // Direct Admission Fees Payment
 // router

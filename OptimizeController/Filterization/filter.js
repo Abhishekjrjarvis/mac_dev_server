fcbc315dcdc501f4364a7fa662b2d60db53cd8a7
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2023-12-10 22:37:56 +0530

    merge v1 with optimized v2 controller with some models changes

--- OptimizeController/Filterization/filter.js
+++ OptimizeController/Filterization/filter.js
@@ -5090,9 +5090,7 @@ exports.renderStudentFeesStatisticsQuery = async(req, res) => {
               .select("className classTitle masterClassName ApproveStudent")
               var custom_classes = []
               for(var cls of classes){
-                console.log(cls?.ApproveStudent)
                 var all_student = await Student.find({ $and: [{ _id: { $in: cls?.ApproveStudent} }]})
-                console.log(all_student?.length)
                 for(var ref of all_student){
                   var all_remain = await RemainingList.find({ student: ref?._id })
                   .populate({

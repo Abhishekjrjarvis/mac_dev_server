fce26152a3e5050e38b40ec6585680d4aae7466f
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2023-12-18 23:22:15 +0530

    Tushar New Origin URL + Ins POST + POST + Library

--- controllers/InstituteAdmin/Post/PostController.js
+++ controllers/InstituteAdmin/Post/PostController.js
@@ -26,7 +26,7 @@ exports.postWithText = async (req, res) => {
       // .populate({ path: "userFollowersList" })
       // .populate({ path: "joinedUserList" });
     const post = new Post({ ...req.body });
-    const taggedPeople = req.body?.people ? JSON?.parse(req.body?.people) : "";
+    const taggedPeople = req.body?.people != "" ? JSON?.parse(req.body?.people) : "";
     if (Array.isArray(taggedPeople)) {
       for (let val of taggedPeople) {
         post.tagPeople.push({

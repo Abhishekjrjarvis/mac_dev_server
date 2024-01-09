fd1e3c07a11903ac3e549e411f907077f2551321
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2024-01-09 19:02:19 +0530

    update New Fees Card . + Review + Fees Collection + Direct Admission + Re - Admission

--- models/Admission/RemainingList.js
+++ models/Admission/RemainingList.js
@@ -32,6 +32,59 @@ const remainingFeeListSchema = new mongoose.Schema({
       ref: "FeeReceipt",
     },
   ],
+  remaining_array: [
+    {
+      appId: { type: mongoose.Schema.Types.ObjectId, ref: "NewApplication" },
+      vehicleId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Vehicle",
+      },
+      remainAmount: { type: Number, default: 0 },
+      status: { type: String, default: "Not Paid" },
+      instituteId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "InstituteAdmin",
+      },
+      installmentValue: { type: String },
+      isEnable: { type: Boolean, default: false },
+      mode: { type: String },
+      originalFee: { type: Number, default: 0 },
+      dueDate: { type: String },
+      exempt_status: { type: String, default: "Not Exempted" },
+      fee_receipt: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "FeeReceipt",
+      },
+      refund_status: { type: String, default: "Not Refunded" },
+      reject_reason: { type: String },
+      receipt_status: {
+        type: String
+      },
+      reason: {
+        type: String
+    },
+    revert_status: {
+        type: String
+    },
+    component: {
+      app: {
+        type: Number,
+        default: 0
+      },
+      gov: {
+        type: Number,
+        default: 0
+      }
+    },
+    cover_status: {
+      type: String
+    },
+    set_off: {
+      type: Number,
+      default: 0
+    }
+    },
+  ],
   status: { type: String, default: "Not Paid" },
   student: {
     type: mongoose.Schema.Types.ObjectId,

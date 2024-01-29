96b71d8a3840b7604667e49b19a30c1169b2df04
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2024-01-29 21:51:58 +0530

    Library Code Updated + QR Generation + User Post Notification

--- OptimizeController/Admission/admissionController.js
+++ OptimizeController/Admission/admissionController.js
@@ -11044,7 +11044,7 @@ exports.renderShiftApplicableToGovernmentQuery = async (req, res) => {
           }
           val.remainAmount -= amount
           val.revert_status = "Applicable Fees Shifted To Government Fees"
-          val.set_off = amount
+          val.set_off += amount
           val.set_off_message = `Applicable Fees set off ${amount} with government fees`
           if (nest_app_card?.remaining_fee >= amount) {
             nest_app_card.remaining_fee -= amount

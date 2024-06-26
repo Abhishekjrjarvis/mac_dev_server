3b5653623ee787f753cc4950fbda951b7bbf7932
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2024-06-26 18:32:16 +0530

    payroll + Split INstallment + Application Pinning + Nespharmacy New Its Own Site + Server + unpin bug + merged application

--- controllers/Edit/studentMember.js
+++ controllers/Edit/studentMember.js
@@ -104,6 +104,7 @@ exports.formEditByClassTeacher = async (req, res) => {
       one_student[`${studentObj}`] = req.body?.student[studentObj];
     }
     one_student.student_dynamic_field = [...req.body?.student_dynamic_field]
+    one_student.application_print = []
     await one_student.save();
     res.status(200).send({
       message: "Student form edited successfully👍",

37c55e740e50755cdc0cf0de4009d6ec06d867fc
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2024-07-31 09:42:34 +0530

    payroll + Split INstallment + Application Pinning + Nespharmacy New Its Own Site + Server + unpin bug + merged application

--- controllers/InstituteAdmin/InstituteController.js
+++ controllers/InstituteAdmin/InstituteController.js
@@ -6733,7 +6733,7 @@ exports.render_edit_student_form_section_checklist_query = async (req, res) => {
               ele.form_checklist_required = form_checklist_required
               await ele.save()
               if (ele?.form_common_key) {
-                let exist = await FormChecklist.findOne({ $and: [{ form: iaf?._id }, { form_checklist_key: ele?.form_common_key }] })
+                let exist = await FormChecklist.findOne({ $and: [{ form: ifs?._id }, { form_checklist_key: ele?.form_common_key }] })
                 exist.form_checklist_visibility = ele?.form_checklist_visibility
                 await exist.save()
               }

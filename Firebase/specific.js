d1c1e6d0568993e8cffe021249f22cff272807b4
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2024-08-13 18:01:44 +0530

    Allot Revert + Excel FML Name of Student + Firebase new json file + New Other Fees + Allot

--- Firebase/specific.js
+++ Firebase/specific.js
@@ -1,64 +1,64 @@
-// var admin = require("firebase-admin");
-// var serviceAccount = require("../Secret/qviple-user-firebase-adminsdk-4qvna-8582f91ae3.json");
-// // if (!admin.apps.length) {
-//   let fb = admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//     databaseURL: "dB_URL"
-//   }, "qviple-user");
-// // }
-// const invokeSpecificRegister = async (type, info, title, id, token) => {
-//   // const yourFirebaseAdminConfig= {};
+var admin = require("firebase-admin");
+var serviceAccount = require("../Secret/qviple-user-firebase-adminsdk-4qvna-8582f91ae3.json");
+// if (!admin.apps.length) {
+  let fb = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "dB_URL"
+  }, "qviple-user");
+// }
+const invokeSpecificRegister = async (type, info, title, id, token) => {
+  // const yourFirebaseAdminConfig= {};
 
-//   //   if (admin.apps.length === 0) {
-//   //     admin.initializeApp({ yourFirebaseAdminConfig });
-//   //   }
+  //   if (admin.apps.length === 0) {
+  //     admin.initializeApp({ yourFirebaseAdminConfig });
+  //   }
 
-//   if (type === "Specific Notification") {
-//     const firebaseToken = token;
+  if (type === "Specific Notification") {
+    const firebaseToken = token;
 
-//     // const messaging = admin.messaging();
+    // const messaging = admin.messaging();
   
-//     const ds = {
-//       // message: {
-//         notification: {
-//           title: `${title}`,
-//           body: `${info}`,
-//         },
-//         data: {
-//           type: `${type}`,
-//           userId: `${id}`,
-//           click_action: "FLUTTER_NOTIFICATION_CLICK",
-//           sound: "default",
-//         },
-//         token: firebaseToken // Replace with the device token you want to send to
-//       // },
-//       // token: firebaseToken // Replace with the device token you want to send to
-//   };
+    const ds = {
+      // message: {
+        notification: {
+          title: `${title}`,
+          body: `${info}`,
+        },
+        data: {
+          type: `${type}`,
+          userId: `${id}`,
+          click_action: "FLUTTER_NOTIFICATION_CLICK",
+          sound: "default",
+        },
+        token: firebaseToken // Replace with the device token you want to send to
+      // },
+      // token: firebaseToken // Replace with the device token you want to send to
+  };
   
-//     // const options = {
-//     //   priority: "high",
-//     //   timeToLive: 60 * 60 * 24,
-//     // };
+    // const options = {
+    //   priority: "high",
+    //   timeToLive: 60 * 60 * 24,
+    // };
 
-//     // try {
-//     fb.messaging().send(ds)
-//       .then(() => {
-//         console.log("Notification sent successfully:")
-//       }).catch((e) => {
-//     // catch (error) {
-//       console.error("Error sending notification:", e);
-//       })
-//   } 
-//   else {
-//   }
-// };
+    // try {
+    fb.messaging().send(ds)
+      .then(() => {
+        console.log("Notification sent successfully:")
+      }).catch((e) => {
+    // catch (error) {
+      console.error("Error sending notification:", e);
+      })
+  } 
+  else {
+  }
+};
 
-// module.exports = invokeSpecificRegister;
+module.exports = invokeSpecificRegister;
 
-// // invokeSpecificRegister(
-// //   "Specific Notification",
-// //   "TESTING NODTICIATION TRIGGERED BY DD ABHISHEK SINGH",
-// //   "New Institute Welcome",
-// //   "630f6d19a8d864c2234fe4cc",
-// //   // user.deviceToken
-// // )
+// invokeSpecificRegister(
+//   "Specific Notification",
+//   "TESTING NODTICIATION TRIGGERED BY DD ABHISHEK SINGH",
+//   "New Institute Welcome",
+//   "630f6d19a8d864c2234fe4cc",
+//   // user.deviceToken
+// )

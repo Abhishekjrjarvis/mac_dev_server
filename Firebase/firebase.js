ce67cfaf30fc6e1061f2c98d0f523fb0e10f58f1
Abhishek Singh <70634372+Abhishekjrjarvis@users.noreply.github.com>
2024-08-14 13:58:07 +0530

    Update firebase.js

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -1,265 +1,144 @@
 var firebase = require("firebase-admin");
 var serviceAccount = require("../Secret/qviple-user-firebase-adminsdk-4qvna-8582f91ae3.json");
 
-const invokeFirebaseNotification = async (
+const invokeMemberTabNotification = (
   type,
   info,
   title,
   id,
   token,
-  pid,
-  aid
+  publisher,
+  notifyData
 ) => {
-  if (!firebase.apps.length) {
-    var fb = firebase.initializeApp({
-        credential: firebase.credential.cert(serviceAccount),
-        databaseURL: "dB_URL"
-      }, "qviple-user");
-  }
-  if (type === "Followers") {
-    const firebaseToken = `${token && token}`;
-    const ds = {
-        notification: {
-          title: `${title}`,
-          body: `${info.notifyContent}`,
-        },
-        data: {
-          type: `${type}`,
-          userId: `${id}`,
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default",
-        },
-        token: firebaseToken
-  };
-
-    fb.messaging().send(ds)
-    .then(() => {
-        console.log("Notification sent successfully:")
-    }).catch((e) => {
-        console.error("Error sending notification:", e);
-    })
-  } else if (type === "Circle") {
-    const firebaseToken = `${token && token}`;
-    const ds = {
-        notification: {
-          title: `${title}`,
-          body: `${info.notifyContent}`,
-        },
-        data: {
-          type: `${type}`,
-          userId: `${id}`,
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default",
-        },
-        token: firebaseToken
-  };
-
-    fb.messaging().send(ds)
-    .then(() => {
-      console.log("Notification sent successfully:")
-    }).catch((e) => {
-        console.error("Error sending notification:", e);
-    })
-  } else if (type === "Staff Approval") {
-    const firebaseToken = `${token && token}`;
-    const ds = {
-        notification: {
-          title: `${title}`,
-          body: `${info.notifyContent}`,
-        },
-        data: {
-          type: `${type}`,
-          userId: `${id}`,
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default",
-        },
-        token: firebaseToken
-  };
-
-    fb.messaging().send(ds)
-    .then(() => {
-      console.log("Notification sent successfully:")
-    }).catch((e) => {
-        console.error("Error sending notification:", e);
-    })
-  } else if (type === "Student Approval") {
-    const firebaseToken = `${token && token}`;
-    const ds = {
-        notification: {
-          title: `${title}`,
-          body: `${info.notifyContent}`,
-        },
-        data: {
-          type: `${type}`,
-          userId: `${id}`,
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default",
-        },
-        token: firebaseToken
-  };
-
-    fb.messaging().send(ds)
-    .then(() => {
-      console.log("Notification sent successfully:")
-    }).catch((e) => {
-        console.error("Error sending notification:", e);
-    })
-  } else if (type === "Designation Allocation") {
-    const firebaseToken = `${token && token}`;
-    const ds = {
-        notification: {
-          title: `${title}`,
-          body: `${info.notifyContent}`,
-        },
-        data: {
-          type: `${type}`,
-          userId: `${id}`,
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default",
-        },
-        token: firebaseToken
-  };
-
-    fb.messaging().send(ds)
-    .then(() => {
-      console.log("Notification sent successfully:")
-    }).catch((e) => {
-        console.error("Error sending notification:", e);
-    })
-  } else if (type === "Announcement") {
-    const firebaseToken = `${token && token}`;
-    const ds = {
-        notification: {
-          title: `${title}`,
-          body: `${info.notifyContent}`,
-        },
-        data: {
-          type: `${type}`,
-          userId: `${id}`,
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default",
-        },
-        token: firebaseToken
-  };
-
-    fb.messaging().send(ds)
-    .then(() => {
-      console.log("Notification sent successfully:")
-    }).catch((e) => {
-        console.error("Error sending notification:", e);
-    })
-  } else if (type === "Student Member Activity") {
-    const firebaseToken = `${token && token}`;
-    const ds = {
-        notification: {
-          title: `${title}`,
-          body: `${info.notifyContent}`,
-        },
-        data: {
-          type: `${type}`,
-          userId: `${id}`,
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default",
-        },
-        token: firebaseToken
-  };
-
-    fb.messaging().send(ds)
-    .then(() => {
-      console.log("Notification sent successfully:")
-    }).catch((e) => {
-        console.error("Error sending notification:", e);
-    })
-  } else if (type === "Staff Member Activity") {
-    const firebaseToken = `${token && token}`;
-    const ds = {
-        notification: {
-          title: `${title}`,
-          body: `${info.notifyContent}`,
-        },
-        data: {
-          type: `${type}`,
-          userId: `${id}`,
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default",
-        },
-        token: firebaseToken
-  };
-
-    fb.messaging().send(ds)
-    .then(() => {
-      console.log("Notification sent successfully:")
-    }).catch((e) => {
-        console.error("Error sending notification:", e);
-    })
-  } else if (type === "Comment") {
-    const firebaseToken = `${token && token}`;
-    const ds = {
-        notification: {
-          title: `${title}`,
-          body: `${info.notifyContent}`,
-        },
-        data: {
-          type: `${type}`,
-          userId: `${id}`,
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default",
-        },
-        token: firebaseToken
-  };
-
-    fb.messaging().send(ds)
-    .then(() => {
-      console.log("Notification sent successfully:")
-    }).catch((e) => {
-        console.error("Error sending notification:", e);
-    })
-  } else if (type === "Answer") {
-    const firebaseToken = `${token && token}`;
-    const ds = {
-        notification: {
-          title: `${title}`,
-          body: `${info.notifyContent}`,
-        },
-        data: {
-          type: `${type}`,
-          userId: `${id}`,
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default",
-        },
-        token: firebaseToken
-  };
-
-    fb.messaging().send(ds)
-    .then(() => {
-      console.log("Notification sent successfully:")
-    }).catch((e) => {
-        console.error("Error sending notification:", e);
-    })
-  } else if (type === "New To Post Feed") {
-    const firebaseToken = `${token && token}`;
-    const ds = {
-        notification: {
-          title: `${title}`,
-          body: `${info.notifyContent}`,
-        },
-        data: {
-          type: `${type}`,
-          userId: `${id}`,
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default",
-        },
-        token: firebaseToken
-  };
-
-    fb.messaging().send(ds)
-    .then(() => {
-      console.log("Notification sent successfully:")
-    }).catch((e) => {
-        console.error("Error sending notification:", e);
-    })
-  } else {
-  }
+  //   if (!firebase.apps.length) {
+  //       var fb = firebase.initializeApp({
+  //           credential: firebase.credential.cert(serviceAccount),
+  //           databaseURL: "dB_URL"
+  //         }, "qviple-user");
+  //     }
+  //   if (type === "Student Activity") {
+      
+  //       const firebaseToken = `${token && token}`;
+  //       const ds = {
+  //           notification: {
+  //             title: `${title}`,
+  //             body: `${info.notifyContent}`,
+  //           },
+  //           data: {
+  //             type: `${type}`,
+  //               userId: `${id}`,
+  //               publisher: `${publisher}`,
+  //               openData: `${notifyData}`,
+  //             click_action: "FLUTTER_NOTIFICATION_CLICK",
+  //             sound: "default",
+  //           },
+  //           token: firebaseToken
+  //     };
+    
+  //       fb.messaging().send(ds)
+  //       .then(() => {
+  //         console.log("Notification sent successfully:")
+  //       }).catch((e) => {
+  //           console.error("Error sending notification:", e);
+  //       })
+  // } else if (type === "Staff Activity") {
+  //   const firebaseToken = `${token && token}`;
+  //       const ds = {
+  //           notification: {
+  //             title: `${title}`,
+  //             body: `${info.notifyContent}`,
+  //           },
+  //           data: {
+  //             type: `${type}`,
+  //               userId: `${id}`,
+  //               publisher: `${publisher}`,
+  //               openData: `${notifyData}`,
+  //             click_action: "FLUTTER_NOTIFICATION_CLICK",
+  //             sound: "default",
+  //           },
+  //           token: firebaseToken
+  //     };
+    
+  //       fb.messaging().send(ds)
+  //       .then(() => {
+  //         console.log("Notification sent successfully:")
+  //       }).catch((e) => {
+  //           console.error("Error sending notification:", e);
+  //       })
+  // } else if (type === "Institute Activity") {
+  //   const firebaseToken = `${token && token}`;
+  //       const ds = {
+  //           notification: {
+  //             title: `${title}`,
+  //             body: `${info.notifyContent}`,
+  //           },
+  //           data: {
+  //             type: `${type}`,
+  //               userId: `${id}`,
+  //               publisher: `${publisher}`,
+  //               openData: `${notifyData}`,
+  //             click_action: "FLUTTER_NOTIFICATION_CLICK",
+  //             sound: "default",
+  //           },
+  //           token: firebaseToken
+  //     };
+    
+  //       fb.messaging().send(ds)
+  //       .then(() => {
+  //         console.log("Notification sent successfully:")
+  //       }).catch((e) => {
+  //           console.error("Error sending notification:", e);
+  //       })
+  // } else if (type === "Admission Status") {
+  //   const firebaseToken = `${token && token}`;
+  //       const ds = {
+  //           notification: {
+  //             title: `${title}`,
+  //             body: `${info.notifyContent}`,
+  //           },
+  //           data: {
+  //             type: `${type}`,
+  //               userId: `${id}`,
+  //               publisher: `${publisher}`,
+  //               openData: `${notifyData}`,
+  //             click_action: "FLUTTER_NOTIFICATION_CLICK",
+  //               sound: "default",
+  //           },
+  //           token: firebaseToken
+  //     };
+    
+  //       fb.messaging().send(ds)
+  //       .then(() => {
+  //         console.log("Notification sent successfully:")
+  //       }).catch((e) => {
+  //           console.error("Error sending notification:", e);
+  //       })
+  // } else if (type === "Meeting Alert") {
+  //   const firebaseToken = `${token && token}`;
+  //       const ds = {
+  //           notification: {
+  //             title: `${title}`,
+  //             body: `${info.notifyContent}`,
+  //           },
+  //           data: {
+  //             type: `${type}`,
+  //               userId: `${id}`,
+  //               publisher: `${publisher}`,
+  //               openData: `${notifyData}`,
+  //             click_action: "FLUTTER_NOTIFICATION_CLICK",
+  //             sound: "default",
+  //           },
+  //           token: firebaseToken
+  //     };
+    
+  //       fb.messaging().send(ds)
+  //       .then(() => {
+  //         console.log("Notification sent successfully:")
+  //       }).catch((e) => {
+  //           console.error("Error sending notification:", e);
+  //       })
+  // } else {
+  // }
 };
 
-module.exports = invokeFirebaseNotification;
+module.exports = invokeMemberTabNotification;

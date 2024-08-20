045737f2425293747b2d2c2db1e5454b0e5f5005
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2024-08-20 14:10:02 +0530

    Allot Revert + Excel FML Name of Student + Firebase new json file + New Other Fees + Allot + Paytm Error Token + Validate Status API Not Tested Functions + New DayBook + Collect Docs API

--- scripts/bankDaybook.js
+++ scripts/bankDaybook.js
@@ -389,12 +389,11 @@ const bankDaybook = async (fid, from, to, bank, payment_type, flow) => {
       bank: bank,
       flow: flow ?? ""
     });
-    let filess = results?.Key
     await unlinkFile(file.path);
     await Promise.all([ bank_acc.save(), finance.save() ])
-    return filess
   });
 
+  return `${name}-bank-daybook.pdf`
   //   console.log(data);
 };
 module.exports = bankDaybook;
--- scripts/hostelBankDaybook.js
+++ scripts/hostelBankDaybook.js
@@ -389,11 +389,10 @@ const hostelBankDaybook = async (fid, hid, from, to, bank, payment_type, flow) =
       bank: bank,
       flow: flow ?? ""
     });
-    let filess = results?.Key
     await unlinkFile(file.path);
     await Promise.all([ bank_acc.save(), finance.save() ])
-    return filess
   });
+  return `${name}-bank-daybook.pdf`
 };
 module.exports = hostelBankDaybook;
 

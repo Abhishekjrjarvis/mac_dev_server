83c466fab20b0a33e1910e7838f48dd3b942300a
Abhishekjrjarvis <skdabhishekjrjarvis10@gmail.com>
2024-09-23 09:58:40 +0530

    Allot Revert + Excel FML Name of Student + Firebase new json file + New Other Fees + Allot + Paytm Error Token + Validate Status API Not Tested Functions + New DayBook + Collect Docs API + CASH_BANK + Subject Crtiteria bug + New Daybook Role + Day Wise Count + APPLICABLE OS + TOTAL OS + Day wise conditionally + Review Application To Allotted Application Validation + Student Name Search iN TXN History + Activity Batch + Add AStudent Validation In Other Fees Not Twice + Bank Wise receipt count excel new modification + Shuffle + studentCastCategory In Excel format + moderators + student roll coll wise + Shuffle Change + Other fees search + export +All Student TAB + Delete Fee Receipt Bug + Announcement Notice + DSE Student Subject + Bank Account Wise Data +

--- OptimizeController/Admission/admissionController.js
+++ OptimizeController/Admission/admissionController.js
@@ -142,6 +142,7 @@ const StudentPreviousData = require("../../models/StudentPreviousData");
 const moment = require("moment");
 const societyAdmissionFeeReceipt = require("../../scripts/societyAdmissionFeeReceipt");
 const FeesCategory = require("../../models/Finance/FeesCategory");
+const { classes_shuffle_func } = require("../../Designation/functions");
 
 exports.retrieveAdmissionAdminHead = async (req, res) => {
   try {
@@ -3565,6 +3566,8 @@ exports.retrieveClassAllotQuery = async (req, res) => {
               await ele.save();
             }
           }
+          await classes_shuffle_func(classes, student);
+
           await Promise.all([
             apply.save(),
             student.save(),
@@ -16730,6 +16733,91 @@ exports.render_one_subject_student_query = async (req, res) => {
           }
         }
       }
+    }
+    const unique = [...new Set(n.map((item) => item._id))];
+    const all = await Student.find({ _id: { $in: unique } })
+      .select(
+        "studentFirstName studentMiddleName studentFatherName studentLastName studentProfilePhoto photoId studentGender studentPhoneNumber studentEmail studentROLLNO studentGRNO"
+      )
+      .populate({
+        path: "user",
+        select: "userLegalName username",
+      });
+    const all_students = await nested_document_limit(page, limit, all);
+    res.status(200).send({
+      message: "Explore All Students Master Query",
+      access: true,
+      student: all_students?.length > 0 ? all_students : [],
+      student_count: unique?.length,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+exports.render_one_subject_all_dse_student_query = async (req, res) => {
+  try {
+    const { sid, aid } = req?.params;
+    const page = req.query.page ? parseInt(req.query.page) : 1;
+    const limit = req.query.limit ? parseInt(req.query.limit) : 10;
+    const skip = (page - 1) * limit;
+    if (!sid)
+      return res.status(200).send({
+        message: "Their is a bug need to fixed immediately",
+        access: false,
+      });
+
+    const one_subject = await SubjectMaster.findById({ _id: sid });
+    const apply = await NewApplication.findById({ _id: aid });
+    // const nums = [aid]
+    // const all_user = await User.find({ applyApplication: { $in: nums } })
+    let numss = [];
+    for (let ele of apply?.confirmedApplication) {
+      numss.push(ele?.student);
+    }
+    for (let ele of apply?.allottedApplication) {
+      numss.push(ele?.student);
+    }
+    let subject_num = [...numss, ...apply?.reviewApplication];
+    const all_student = await Student.find({ _id: { $in: subject_num } })
+      .select(
+        "studentFirstName studentMiddleName studentFatherName studentLastName studentProfilePhoto photoId studentGender studentPhoneNumber studentEmail studentROLLNO studentGRNO"
+      )
+      .populate({
+        path: "user",
+        select: "userLegalName username",
+      })
+      .populate({
+        path: "student_optional_subject",
+        select: "subjectName",
+      })
+      .populate({
+        path: "major_subject",
+        select: "subjectName",
+      })
+      .populate({
+        path: "nested_subject",
+        select: "subjectName",
+      });
+    var n = [];
+    for (let val of all_student) {
+      // for (let ele of val?.student_optional_subject) {
+      //   if (`${ele?._id}` === `${one_subject?._id}`) n.push(val);
+      // }
+      for (let val of all_student) {
+        for (let ele of val?.major_subject) {
+          if (`${ele?._id}` === `${one_subject?._id}`) {
+            n.push(val);
+          }
+        }
+      }
+      // for (let val of all_student) {
+      //   for (let ele of val?.nested_subject) {
+      //     if (`${ele?._id}` === `${one_subject?._id}`) {
+      //       n.push(val);
+      //     }
+      //   }
+      // }
     }
     const unique = [...new Set(n.map((item) => item._id))];
     const all = await Student.find({ _id: { $in: unique } })

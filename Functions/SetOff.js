8cd6a405ea3407ae6e324867cb05d5640b835d18
Abhishek Singh <70634372+Abhishekjrjarvis@users.noreply.github.com>
2024-10-18 13:17:54 +0530

    Update SetOff.js

--- Functions/SetOff.js
+++ Functions/SetOff.js
@@ -33,7 +33,7 @@ exports.applicable_pending_calc = async (arr) => {
     for (var s_args of arr) {
       if (s_args?.remainingFeeList?.length > 0) {
         for (var r_args of s_args?.remainingFeeList) {
-          s_args.applicable_fees_pending += r_args?.applicable_card?.remaining_fee
+          s_args.applicable_fees_pending += r_args?.applicable_card?.remaining_fee == 0 ? 1 : r_args?.applicable_card?.remaining_fee
           s_args.government_fees_pending += r_args?.government_card?.remaining_fee
         }
       }
